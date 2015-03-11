fe2e13338ecd7ac53b8d56ec18041421eb6bfa60
Salim KAYABAŞI <salim.kayabasi@gmail.com>
2015-03-12 01:23:02 +0200

    use location key from prefs while fetching weather data

--- app/src/main/java/com/gdgistanbul/sunshine/ForecastFragment.java
+++ app/src/main/java/com/gdgistanbul/sunshine/ForecastFragment.java
@@ -1,9 +1,11 @@
 package com.gdgistanbul.sunshine;
 
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.net.Uri;
 import android.os.AsyncTask;
 import android.os.Bundle;
+import android.preference.PreferenceManager;
 import android.support.v4.app.Fragment;
 import android.text.format.Time;
 import android.util.Log;
@@ -58,7 +60,10 @@ public class ForecastFragment extends Fragment {
         int id = item.getItemId();
         if (id == R.id.action_refresh) {
             FetchWeatherTask weatherTask = new FetchWeatherTask();
-            weatherTask.execute("Istanbul");
+            SharedPreferences prefs = PreferenceManager.getDefaultSharedPreferences(getActivity());
+            String location = prefs.getString(getString(R.string.pref_location_key),
+                    getString(R.string.pref_location_default));
+            weatherTask.execute(location);
             return true;
         }
         return super.onOptionsItemSelected(item);
